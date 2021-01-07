0b5d1bf23ec6ca15077dad4ba8f332c98040bd83
MauriceConrad <conr.maur@ooglemail.com>
2021-01-07 15:39:25 +0100

    update

--- src/components/Spend.vue
+++ src/components/Spend.vue
@@ -5,10 +5,133 @@
     <SpendWiget :current="current" :goals="[10000, 25000, 50000, 100000, 500000]" />
     <br><br>
     <div class="description">
+      <div class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28.621 28.621">
+          <g>
+            <path d="M14.311,0c-6.904,0-12.5,5.596-12.5,12.5c0,4.723,2.618,8.828,6.48,10.955l-0.147,5.166l5.898-3.635 C14.131,24.988,14.22,25,14.311,25c6.904,0,12.5-5.596,12.5-12.5C26.811,5.596,21.215,0,14.311,0z M15.903,19.784h-3.203V9.474 h3.203V19.784z M14.28,8.211c-1.013,0-1.687-0.718-1.665-1.604c-0.021-0.927,0.652-1.624,1.686-1.624 c1.033,0,1.689,0.697,1.71,1.624C16.01,7.493,15.332,8.211,14.28,8.211z"/>
+          </g>
+        </svg>
+      </div>
       Um unseren <strong>Wahlkampf</strong> zu ermöglichen, sind wir auf <strong>Spenden</strong> angewiesen. Dadurch gewährleisten wir unsere Unabhängigkeit.
       <br><br>
       Spenden an Wahler:innenvereinigungen sind besonders <strong>steuerlich abzugsfähig</strong>. <strong>50%</strong> werden direkt von der Einkommensteuer abgezogen. Von jährlich bis zu <strong>1.650 €</strong> bei Ledigen, bzw <strong>3.300 €</strong> bei gemeinsam Veranlagten.
     </div>
+
+    <div class="actions-main">
+      <div class="actions-view">
+
+        <ul class="actions-list">
+          <li>
+            <div class="inner">
+              <h3>Überweisung</h3>
+              <div class="icon-container">
+                <div class="icon">
+                  <svg width="100%" height="100%" viewBox="0 0 3711 3711" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                    <g transform="matrix(4.16667,0,0,4.16667,0,0)">
+                      <g>
+                        <path d="M208.1,180.56L563.1,83.66L544.3,45.66C532,20.96 502,10.76 477.3,23.06L159.5,180.56L208.1,180.56Z" style="fill:rgb(149,118,70);fill-rule:nonzero;"/>
+                        <path d="M673.3,86.46C668.901,86.46 664.5,87.06 660.1,88.26L576.701,111.06L322,180.56L737.1,180.56L721.5,123.36C715.5,101.06 695.3,86.46 673.3,86.46Z" style="fill:rgb(172,140,89);fill-rule:nonzero;"/>
+                      </g>
+                      <path d="M789.2,215.56L50.1,215.56C34.3,215.56 20.2,222.86 11,234.36C6.8,239.66 3.6,245.76 1.8,252.46C0.7,256.66 0,261.06 0,265.56L0,822.66C0,850.261 22.4,872.66 50,872.66L789.1,872.66C816.701,872.66 839.1,850.261 839.1,822.66L839.1,683.16L542.4,683.16C495.5,683.16 457.4,645.06 457.4,598.16L457.4,486.96C457.4,463.96 466.599,443.061 481.5,427.761C494.7,414.16 512.4,404.96 532.2,402.66C535.5,402.261 538.9,402.06 542.3,402.06L839.1,402.06L839.1,265.56C839.2,237.96 816.8,215.56 789.2,215.56Z" style="fill:rgb(191,150,86);fill-rule:nonzero;"/>
+                      <path d="M874.2,449.86C869.2,445.26 863.3,441.76 856.7,439.46C851.599,437.761 846.2,436.761 840.5,436.761L542.4,436.761C514.799,436.761 492.4,459.16 492.4,486.761L492.4,598.06C492.4,625.66 514.799,648.06 542.4,648.06L840.5,648.06C846.2,648.06 851.6,647.06 856.7,645.36C863.3,643.16 869.2,639.56 874.2,634.96C884.2,625.86 890.5,612.66 890.5,598.061L890.5,486.761C890.5,472.16 884.2,458.959 874.2,449.86ZM646.8,552.36C646.8,566.16 635.6,577.36 621.8,577.36L605.2,577.36C591.4,577.36 580.2,566.16 580.2,552.36L580.2,535.76C580.2,527.76 583.9,520.659 589.8,516.159C594.1,512.859 599.401,510.76 605.2,510.76L621.8,510.76C635.6,510.76 646.8,521.959 646.8,535.76L646.8,552.36Z" style="fill:rgb(172,140,89);fill-rule:nonzero;"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+              <span>
+                Spenden an unser Konto bei der GLS Bank kommen ohne Gebühr in voller Höhe bei uns an.
+                <br><br>
+                Empfänger: <strong>Klimaliste RLP e.V.</strong>
+                <br>
+                IBAN: <strong>DE13430609671090283300</strong>
+                <br>
+                BIC: <strong>GENODEM1GLS</strong>
+                <br>
+              </span>
+            </div>
+          </li>
+          <li>
+            <div class="inner">
+              <h3>PayPal</h3>
+              <div class="icon-container">
+                <div class="icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.001 512.001">
+                    <g>
+                      <path style="fill:#03A9F4;" d="M425.457,117.739c-3.121-1.838-6.961-1.966-10.197-0.341c-3.231,1.629-5.416,4.786-5.803,8.384 c-0.384,3.499-0.981,6.997-1.728,10.667c-20.885,94.784-62.827,140.885-128.256,140.885h-96c-5.062,0.009-9.42,3.574-10.432,8.533 l-32,149.995l-5.717,38.187c-3.287,17.365,8.125,34.107,25.489,37.394c1.915,0.362,3.858,0.549,5.807,0.558h64.213 c14.718,0.045,27.55-10,31.04-24.299l25.941-103.701h55.659c65.685,0,111.083-52.373,127.829-147.477l0,0 C482.356,191.238,464.068,143.856,425.457,117.739z"/>
+                      <path style="fill:#283593;" d="M405.339,38.017c-21.078-23.909-51.327-37.731-83.2-38.016h-176.64 C119.064-0.141,96.558,19.2,92.721,45.355L37.873,411.243c-2.627,17.477,9.41,33.774,26.887,36.402 c1.586,0.239,3.189,0.357,4.793,0.356h81.92c5.062-0.009,9.42-3.574,10.432-8.533l30.187-140.8h87.467 c75.904,0,126.059-53.056,149.099-157.867c0.926-4.178,1.638-8.4,2.133-12.651C436.139,95.815,426.81,62.778,405.339,38.017z"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+              <span>
+                Spenden über PayPal können direkt an das Vereinskonto unter <a href="https://paypal.me/klimalisterlp" target="_blank">finanzen@klimalisterlp.de</a> gespendet werden. Das Geld kommt uns, abzüglich der PayPal-Gebühren (0,35 € + 2,5%), direkt zu Gute.
+              </span>
+              <br><br>
+              <a href="http://paypal.me/klimalisterlp" target="_blank">
+                <button class="btn">PayPal Spenden</button>
+              </a>
+            </div>
+          </li>
+          <li>
+            <div class="inner">
+              <h3>Crowd-Funding</h3>
+              <div class="icon-container">
+                <div class="icon">
+                  <svg width="100%" height="100%" viewBox="0 0 2746 2979" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="matrix(3.125,0,0,3.125,0,0)">
+                      <g transform="matrix(1,0,0,1,-61.2192,-86.4695)">
+                        <g transform="matrix(0.721238,0,0,0.721238,138.589,228.118)">
+                          <g transform="matrix(5.66594,0,0,5.66594,-25.443,11.8941)">
+                            <path d="M96,0C60.906,0.039 32.039,28.906 32,64C32,83.536 40.419,95.152 49.333,107.451C51.957,111.072 54.67,114.815 57.233,118.877C60.23,123.613 62.67,131.7 63.77,136L128.23,136C129.33,131.694 131.771,123.612 134.763,118.877C137.33,114.815 140.043,111.077 142.663,107.451C151.581,95.152 160,83.536 160,64C159.961,28.906 131.094,0.039 96,0Z" style="fill:rgb(255,225,0);fill-rule:nonzero;"/>
+                          </g>
+                          <g transform="matrix(0.233564,0,0,2.62969,402.531,-1051.02)">
+                            <path d="M852.646,805.034C852.646,792.576 738.774,782.463 598.516,782.463L394.42,782.463C254.162,782.463 140.29,792.576 140.29,805.034C140.29,817.491 254.162,827.605 394.42,827.605L598.516,827.605C738.774,827.605 852.646,817.491 852.646,805.034Z" style="fill:rgb(132,132,132);"/>
+                          </g>
+                          <g transform="matrix(0.603218,0,0,2.10069,219.009,-874.172)">
+                            <path d="M852.646,805.034C852.646,792.576 817.425,782.463 774.042,782.463L218.894,782.463C175.512,782.463 140.29,792.576 140.29,805.034C140.29,817.491 175.512,827.605 218.894,827.605L774.042,827.605C817.425,827.605 852.646,817.491 852.646,805.034Z" style="fill:rgb(211,211,211);"/>
+                          </g>
+                          <g transform="matrix(0.603218,0,0,2.10069,219.009,-684.51)">
+                            <path d="M852.646,805.034C852.646,792.576 817.425,782.463 774.042,782.463L218.894,782.463C175.512,782.463 140.29,792.576 140.29,805.034C140.29,817.491 175.512,827.605 218.894,827.605L774.042,827.605C817.425,827.605 852.646,817.491 852.646,805.034Z" style="fill:rgb(211,211,211);"/>
+                          </g>
+                          <g transform="matrix(0.603218,0,0,2.10069,219.009,-779.341)">
+                            <path d="M852.646,805.034C852.646,792.576 817.425,782.463 774.042,782.463L218.894,782.463C175.512,782.463 140.29,792.576 140.29,805.034C140.29,817.491 175.512,827.605 218.894,827.605L774.042,827.605C817.425,827.605 852.646,817.491 852.646,805.034Z" style="fill:rgb(200,200,200);"/>
+                          </g>
+                        </g>
+                        <g transform="matrix(1,0,0,1,-0.203857,129.527)">
+                          <g transform="matrix(1.49111,0.384034,-0.384034,1.49111,-199.192,-294.266)">
+                            <path d="M617.042,98.185C647.193,99.93 703.617,118.262 720.925,166.522" style="fill:none;stroke:rgb(255,194,0);stroke-width:32.47px;"/>
+                          </g>
+                          <g transform="matrix(2.3541,0.606295,-0.625616,2.42912,-678.029,-630.67)">
+                            <path d="M617.042,98.185C647.193,99.93 703.617,118.262 720.925,166.522" style="fill:none;stroke:rgb(255,194,0);stroke-width:20.24px;"/>
+                          </g>
+                          <g transform="matrix(0.0693507,-1.53821,1.53821,0.0693507,3.62893,1203.81)">
+                            <path d="M617.042,98.185C647.193,99.93 703.617,118.262 720.925,166.522" style="fill:none;stroke:rgb(255,194,0);stroke-width:32.47px;"/>
+                          </g>
+                          <g transform="matrix(0.109488,-2.42845,2.50584,0.112977,-227.172,1741.56)">
+                            <path d="M617.042,98.185C647.193,99.93 703.617,118.262 720.925,166.522" style="fill:none;stroke:rgb(255,194,0);stroke-width:20.24px;"/>
+                          </g>
+                        </g>
+                        <g transform="matrix(354.043,0,0,354.043,385.935,639.555)">
+                            <path d="M0.454,-0.474L0.617,-0.564C0.603,-0.586 0.587,-0.607 0.57,-0.626C0.552,-0.645 0.53,-0.661 0.505,-0.675C0.48,-0.689 0.449,-0.699 0.412,-0.705L0.418,-0.818L0.264,-0.818L0.27,-0.704C0.228,-0.697 0.192,-0.683 0.161,-0.664C0.13,-0.645 0.107,-0.622 0.09,-0.595C0.073,-0.568 0.064,-0.54 0.064,-0.51C0.064,-0.475 0.071,-0.444 0.085,-0.419C0.098,-0.393 0.116,-0.371 0.137,-0.353C0.158,-0.335 0.179,-0.32 0.202,-0.309C0.224,-0.297 0.244,-0.288 0.261,-0.281C0.302,-0.264 0.335,-0.251 0.359,-0.242C0.382,-0.232 0.394,-0.218 0.394,-0.201C0.394,-0.186 0.389,-0.175 0.38,-0.169C0.37,-0.162 0.353,-0.158 0.33,-0.158C0.297,-0.158 0.268,-0.167 0.244,-0.184C0.219,-0.2 0.194,-0.228 0.168,-0.266L0.02,-0.158C0.033,-0.133 0.051,-0.11 0.077,-0.088C0.101,-0.065 0.131,-0.046 0.164,-0.029C0.197,-0.012 0.233,-0.001 0.27,0.005L0.264,0.112L0.417,0.112L0.411,0.004C0.476,-0.006 0.529,-0.028 0.568,-0.063C0.607,-0.097 0.627,-0.143 0.627,-0.2C0.627,-0.259 0.611,-0.306 0.578,-0.342C0.545,-0.377 0.502,-0.405 0.448,-0.424C0.42,-0.435 0.394,-0.444 0.37,-0.451C0.345,-0.458 0.326,-0.467 0.311,-0.476C0.296,-0.485 0.289,-0.498 0.289,-0.514C0.289,-0.525 0.294,-0.535 0.303,-0.545C0.312,-0.554 0.326,-0.559 0.346,-0.559C0.365,-0.559 0.384,-0.552 0.403,-0.539C0.422,-0.525 0.439,-0.503 0.454,-0.474Z" style="fill:rgb(255,194,0);fill-rule:nonzero;"/>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+              <span>
+                Unsere Crowd-Funding Kampagne kommt fast ohne Gebühren bei uns an. Das Geld steht allerdings immer etwas verzögert zur Verfügung. Dafür ist der Prozess sehr einfach.
+                <br><br>
+                <a href="https://www.gofundme.com/f/klimaliste-rlp" target="_blank">
+                  <button class="btn">Crowd-Funding Kampagne</button>
+                </a>
+              </span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
 
   </div>
 </template>
@@ -20,7 +143,7 @@
     margin: 0 auto;
     box-sizing: border-box;
     font-family: 'Montserrat';
-    padding: 60px 0;
+    padding: 60px 0 0;
   }
   h2 {
     font-weight: 300;
@@ -39,6 +162,218 @@
     padding: 10px 10px;
     border-radius: 5px;
     font-weight: 300;
+  }
+  .description .icon {
+    width: 30px;
+    height: 30px;
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .actions-main {
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
+
+  .actions-info {
+    max-width: 800px;
+    margin: 20px auto 20px;
+    background-color: rgb(244, 244, 244);
+    padding: 10px;
+    border-radius: 3px;
+    font-size: 1.2em;
+  }
+  .actions-info .icon {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+  }
+  .actions-info strong {
+    font-weight: 600;
+  }
+
+
+  .actions-view {
+    margin: 40px 0;
+
+  }
+  .actions-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+  .actions-list > li {
+    flex: 1;
+  }
+  @media screen and (max-width: 600px) {
+    .actions-list {
+      display: block;
+    }
+    .actions-list > li {
+      padding: 20px 0;
+      border-top: 1px solid rgb(217, 217, 217);
+    }
+  }
+  .actions-list > li .inner {
+    max-width: 300px;
+    text-align: center;
+    margin: auto;
+  }
+  @media screen and (max-width: 600px) {
+    .actions-list > li .inner {
+      max-width: 100%;
+
+    }
+  }
+  .actions-list > li .inner h3 {
+    font-size: 1.8em;
+    margin: 0;
+  }
+  .actions-list > li .inner .icon-container {
+    width: 100%;
+    text-align: center;
+    margin: 10px 0 20px;
+  }
+  .actions-list > li .inner .icon-container .icon {
+    height: 80px;
+    background-color: rgb(244, 244, 244);
+    width: 80px;
+    padding: 25px;
+    border-radius: 50%;
+
+    display: inline-block;
+
+  }
+  .actions-list > li .inner .icon > svg {
+    height: 100%;
+    width: 100%;
+  }
+
+
+  .actions-list > li .btn {
+    background-color: #44aa66;
+    color: #fff;
+    font-weight: 400;
+    font-size: 1rem;
+    border: none;
+    padding: 7px 12px;
+  }
+
+
+
+
+  .topic-list {
+    max-width: 800px;
+    margin: auto;
+
+    box-sizing: border-box;
+    width: calc(100% - 0px);
+  }
+  .topic-list ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .topic-list > ul > li {
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+  .topic-list > ul > li .icon-container {
+    flex: none;
+    order: 1;
+  }
+  @media screen and (max-width: 600px) {
+    .topic-list > ul > li {
+      display: block;
+    }
+  }
+  .topic-list > ul > li .icon-container:before {
+    vertical-align: middle;
+    display: inline-block;
+    height: 100%;
+    width: 0;
+    content: "";
+  }
+
+  .topic-list > ul > li .icon-container .icon {
+    display: inline-block;
+    vertical-align: middle;
+
+  }
+
+  .topic-list > ul > li .description {
+    flex: 1;
+    text-align: left;
+    padding: 0 20px;
+    order: 2;
+  }
+  .topic-list > ul > li:nth-child(even) .icon-container {
+    order: 2;
+  }
+  .topic-list > ul > li:nth-child(even) .description {
+    order: 1;
+    text-align: right;
+  }
+
+
+
+  @media screen and (max-width: 600px) {
+    .topic-list > ul > li .description {
+
+    }
+  }
+  .topic-list > ul > li .description:before {
+    vertical-align: middle;
+    display: inline-block;
+    height: 100%;
+    width: 0;
+    content: "";
+  }
+  .topic-list > ul > li .icon .icon-wrapper {
+    background-color: #e9e9e9;
+    display: inline-block;
+    padding: 25px;
+    box-sizing: border-box;
+    border-radius: 50%;
+  }
+  .topic-list > ul > li .icon .icon-wrapper > .icon {
+    height: 150px;
+    width: 150px;
+  }
+  .topic-list > ul > li .description .description-inner {
+    vertical-align: middle;
+    display: inline-block;
+  }
+  .topic-list > ul > li .description .title {
+    margin: 0;
+    font-size: 1.5em;
+    font-weight: 500;
+  }
+  .topic-list > ul > li .description .text {
+    text-align: left;
+    font-weight: 300;
+  }
+
+
+  .title-topic {
+    font-size: 2em;
+  }
+
+  .btn {
+    cursor: pointer;
+    font-size: 1rem;
+    color: #fff;
+    border: none;
+    background-color: #D60078;
+    padding: 8px 13px;
+  }
+  .btn-large {
+    padding: 15px 28px;
+    font-size: 1.6rem;
+  }
+  .btn-large {
+    box-shadow: 6px 6px 0px #a2005b;
   }
 
 
