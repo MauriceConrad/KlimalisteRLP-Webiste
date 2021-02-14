d764f8721ed764fc010134dcc3c441786d3d0244
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-02-14 17:03:20 +0100

    Fix Layout

--- src/components/Spend.vue
+++ src/components/Spend.vue
@@ -15,15 +15,19 @@
 
     <SpendWiget :current="current" :goals="[5000, 10000, 25000, 50000, 100000, 500000]" />
     <br><br>
-    <div class="topic-content-inner">
+    <div class="description">
+      <p>
       Wir sind sehr stolz darauf, keinen Mitgliedbeitrag zu erheben, um allen Menschen, unabhängig ihres Einkommens, eine Mitgliedschaft zu ermöglichen.
 Deshalb sind wir aber umso mehr auf Spenden von Privatpersonen angewiesen, um unseren <strong>Wahlkampf</strong> zu finanzieren. Dadurch gewährleisten wir unsere Unabhängigkeit. Als junge Wählervereinigung nehmen wir noch nicht an der Wahlkampfkostenerstattung teil.
-      <br><br>
+      </p>
+      <p>
       Aktuell sind 50.000€ unser Ziel, um einen effektiven Wahlkampf durchzuführen. Natürlich hilft es uns noch mehr, wenn wir ein höheres Budget zur Verfügung haben.
       Egal wie hoch der Betrag ist: Jede Spende hilft uns weiter.
-      <br><br>
+      </p>
+      <p>
       Spenden an Wähler*innenvereinigungen sind besonders <strong>steuerlich abzugsfähig</strong>. <strong>50%</strong> werden direkt von der Einkommensteuer abgezogen. Von jährlich bis zu <strong>1.650€</strong> bei Ledigen, bzw <strong>3.300€</strong> bei gemeinsam Veranlagten.
       Beispiel: Es reduziert eine Spende von 1000€, die zu zahlende Einkommenssteuer um 500€. So zahlen Sie effektiv 500€ und es kommen trotzdem 1000€ bei uns, zur Finanzierung des Wahlkampfes, an.
+    </p>
     </div>
 
     <div class="actions-main">
