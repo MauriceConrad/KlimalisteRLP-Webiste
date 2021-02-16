a24d6b7145fa90da3f4ff98af470901ab92638e4
Maurice Conrad <conr.maur@ooglemail.com>
2021-02-16 17:46:38 +0100

    addes suppporters

---
+++ data/candidates/ulla/Candidate.js
@@ -0,0 +1,17 @@
+import Avatar from './avatar.jpeg';
+import Background from './bg.jpeg';
+import About from './aboutme.md';
+
+export default {
+  name: "Ulla",
+  avatar: Avatar,
+  bg: Background,
+  bgPos: 'center 42%',
+  bgBlur: '30px',
+  pos: ['top', 'center'],
+  description: `
+    Klimaschützerin
+  `,
+  job: '',
+  about: About
+}
