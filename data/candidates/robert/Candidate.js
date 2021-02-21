c82d7aefce3ff138f75e27535764ca3348fcb92c
Robert Pazer <53620495+RobertPazer@users.noreply.github.com>
2021-02-21 18:20:02 +0100

    Candidate Map update
    Candidate Map Update

--- data/candidates/robert/Candidate.js
+++ data/candidates/robert/Candidate.js
@@ -3,7 +3,7 @@ import Background from './bg.jpeg';
 import About from './aboutme.md';
 
 export default {
-  name: "Robert",
+  name: "Robert Pazer",
   avatar: Avatar,
   bg: Background,
   bgPos: 'center 42%',
