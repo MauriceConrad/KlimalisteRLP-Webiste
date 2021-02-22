035a177eab7a360cd07b035d6c74a067bf606672
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-02-22 08:07:57 +0100

    Kandidat*innen-Gespräch ergänzt

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -227,8 +227,22 @@
               href: 'https://www.twitch.tv/klimalisterlp',
               label: 'Twitch LiveStream'
             }]
-          },/*
+          },
           {
+            title: 'Spitzenkandidat Maurice Conrad: Kandidat*innen-Gespräch live und interaktiv',
+            location: 'Livestream',
+            date: new Date('2021-02-28T19:00:00'),
+            description: 'In diesem LiveStream könnt ihr unseren Kandidat*innen alle Fragen zu unserem Wahlprogramm - dem Klimaplan, zum 1,5-Grad-Ziel für Rheinland-Pfalz und zu allen weiteren Themen stellen und mit ihnen diskutieren. Im ersten Stream sprechen wir dazu mit Maurice Conrad. Er ist unsere Spitzenkandidatin und Informatiker.',
+            actionLink: [{
+              href: 'https://www.youtube.com/watch?v=bk1Y9aF-yo8',
+              label: 'YouTube LiveStream'
+            },
+            {
+              href: 'https://www.twitch.tv/klimalisterlp',
+              label: 'Twitch LiveStream'
+            }]
+          },
+/*       {
             title: 'Transformation der Energiewirtschaft',
             location: 'Livestream',
             date: new Date('2021-03-01T18:00:00'),
