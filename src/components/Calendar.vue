d3931936bd84d1583a3e179ef0a336880bcf5674
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-10 12:46:01 +0100

    Beendetes Gewinnspiel entfernt

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -55,19 +55,6 @@
         </a>
       </div>
     </div>
-    <div class="challange-description">
-      <h3>Finde die Lücke in unserem Klimapan!</h3>
-      <p>
-        Du hast politische Klimaschutzmaßnahmen für RLP entdeckt, die noch nicht im Klimaplan enthalten sind?
-        <br>Dann schicke deinen Maßnahmenvorschlag mit einer kurzen Erklärung an <a href="mailto:inhalt@klimalisterlp.de">inhalt@klimalisterlp.de</a> mit dem Betreff <strong>"Wettbewerb Klimaliste"</strong>.
-        <br>
-        Aus den Einsendungen losen wir 3 Gewinner, die jeweils einen 100 € Gutschein für einen regionalen Bio- oder Unverpacktladen ihrer Wahl bekommen sowie einen Klimalisten-Pulli.
-        <br>
-        <strong>Deadline: Sonntag, 07.03.2021 um 12 Uhr</strong>
-
-      </p>
-      
-    </div>
   </div>
 </template>
 
