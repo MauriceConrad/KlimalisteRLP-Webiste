c354fc74724566f7ecf371c47dc45f8a508c56a3
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-12 23:12:37 +0100

    Info-Stand in Ludwigshafen ergänzt

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -218,6 +218,12 @@
             description: 'Wir informieren euch über die Kernpunkte unseres Klimaplan. Außerdem könnt ihr uns eure Fragen stellen und mit uns diskutieren. Natürlich Corona-konform! 10:00-17:00 Uhr'
           },
           {
+            title: 'Info-Stand',
+            location: 'Ludwigshafen, Parkinsel',
+            date: new Date('2021-03-13T10:00:00'),
+            description: 'Wir informieren euch über die Kernpunkte unseres Klimaplan. Außerdem könnt ihr uns eure Fragen stellen und mit uns diskutieren. Natürlich Corona-konform! 10:00-17:00 Uhr'
+          },
+          {
             title: 'Spitzenduo Beatrice Bednarz und Maurice Conrad im Kandidat*innen-Gespräch live und interaktiv',
             location: 'Livestream',
             date: new Date('2021-03-13T19:00:00'),
